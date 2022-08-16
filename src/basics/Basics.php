8206a7dd81785c9811cd11336763060fed7e5771
pashkalalala <pasharoshko100@gmail.com>
2022-08-16 20:28:51 +0300

    fix basics after review

--- src/basics/Basics.php
+++ src/basics/Basics.php
@@ -6,6 +6,8 @@ class Basics implements BasicsInterface
 {
     public BasicsValidator $validator;
     
+    const DIV_PARTS = 2;
+    
     public function __construct(BasicsValidator $validator)
     {
         $this->validator = $validator;
@@ -14,42 +16,37 @@ class Basics implements BasicsInterface
     public function getMinuteQuarter(int $minute): string
     {
         $this->validator->isMinutesException($minute);
-        
-        if ($minute >= 1 && $minute <= 15 ) {
-            return "first";
-        } elseif ($minute > 15 && $minute <= 30) {
-            return "second";
-        } elseif ($minute > 30 && $minute <= 45) {
-            return "third";
-        } else {
-            return "fourth";
+    
+        if ($minute > 0 && $minute <= 15) {
+            return 'first';
         }
+    
+        if ($minute > 15 && $minute <= 30) {
+            return 'second';
+        }
+    
+        if ($minute > 30 && $minute <= 45) {
+            return 'third';
+        }
+    
+        return 'fourth';
     }
     
     public function isLeapYear(int $year): bool
     {
         $this->validator->isYearException($year);
     
-        if ($year % 4 !== 0) {
-            return false;
-        } elseif ($year % 100 !== 0) {
-            return true;
-        } elseif ($year % 400 !== 0) {
-            return false;
-        } else {
-            return true;
-        }
+        return ((($year % 4) == 0) && ((($year % 100) != 0) || (($year % 400) == 0)));
     }
     
     public function isSumEqual(string $input): bool
     {
-        $this->validator->isYearException($input);
+        $this->validator->isValidStringException($input);
         
-        $arr = str_split($input);
-        $sum1 = array_sum(array_slice($arr, 0, 3));
-        $sum2 = array_sum(array_slice($arr, 3));
+        $numbers = str_split($input);
+        $partsOfArray = array_chunk($numbers, count($numbers) / self::DIV_PARTS);
         
-        return $sum1 == $sum2;
+        return array_sum($partsOfArray[0]) == array_sum($partsOfArray[1]);
     }
     
 }
